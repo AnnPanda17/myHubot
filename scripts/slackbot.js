aa419cf11033e7847645302230e6f89c4d4dd07d
annpanda17 <aliu@zendesk.com>
2017-09-25 20:38:31 -0700

    testing

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -1,6 +1,20 @@
 module.exports = function (robot) {
 
+/*What should my bot do?
+Loops though gifs and sends out a puppy
+Only responds to /puppy time/
+
+/pup fact/
+/pup bark/
+- Respond woof woof
+
+*/
+
   robot.hear(/badger!/, function (res) {
       return res.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS");
     });
+
+  robot.respond(/talk!/, function (res) {
+        return res.send("WOOF WOOF");
+      });
 };
